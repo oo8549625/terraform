a81b552288f0603d91b9faf8d079a9a831a3177b
owen <owen@owendeMacBook-Pro.local>
2023-02-04 17:59:38 +0800

    安裝minio loki

--- helm.tf
+++ helm.tf
@@ -7,6 +7,7 @@ provider "helm" {
 resource "helm_release" "nginx" {
   name      = "nginx"
   namespace = "default"
+  create_namespace = true
   repository = "https://charts.bitnami.com/bitnami"
   chart     = "nginx"
   version   = "13.2.23"
@@ -15,4 +16,35 @@ resource "helm_release" "nginx" {
   values = [
     "${file("nginx-values.yaml")}"
   ]
+  cleanup_on_fail = true
+}
+
+resource "helm_release" "minio" {
+  name      = "minio"
+  namespace = "default"
+  create_namespace = true
+  repository = "https://charts.bitnami.com/bitnami"
+  chart     = "minio"
+  version   = "12.1.3"
+  timeout   = 300
+  wait_for_jobs = true
+  values = [
+    "${file("minio-values.yaml")}"
+  ]
+  cleanup_on_fail = true
+}
+
+resource "helm_release" "loki" {
+  name      = "loki-distributed"
+  namespace = "default"
+  create_namespace = true
+  repository = "https://grafana.github.io/helm-charts"
+  chart     = "loki-distributed"
+  version   = "0.69.4"
+  timeout   = 1800
+  wait_for_jobs = true
+  values = [
+    "${file("loki-values.yaml")}"
+  ]
+  cleanup_on_fail = true
 }
