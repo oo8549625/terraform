20cc9fe76fa0b1a87bf4498385348491bc0e2e93
owen <owen@owendeMacBook-Pro.local>
2023-02-06 10:25:56 +0800

    remove-loki-minio

--- helm.tf
+++ helm.tf
@@ -19,32 +19,34 @@ resource "helm_release" "nginx" {
   cleanup_on_fail = true
 }
 
-resource "helm_release" "minio" {
-  name      = "minio"
-  namespace = "default"
-  create_namespace = true
-  repository = "https://charts.bitnami.com/bitnami"
-  chart     = "minio"
-  version   = "12.1.3"
-  timeout   = 300
-  wait_for_jobs = true
-  values = [
-    "${file("minio-values.yaml")}"
-  ]
-  cleanup_on_fail = true
-}
+# resource "helm_release" "minio" {
+#   name      = "minio"
+#   namespace = "default"
+#   create_namespace = true
+#   repository = "https://charts.bitnami.com/bitnami"
+#   chart     = "minio"
+#   version   = "12.1.3"
+#   timeout   = 300
+#   wait_for_jobs = true
+#   values = [
+#     "${file("minio-values.yaml")}"
+#   ]
+#   cleanup_on_fail = true
+# }
 
-resource "helm_release" "loki" {
-  name      = "loki-distributed"
-  namespace = "default"
-  create_namespace = true
-  repository = "https://grafana.github.io/helm-charts"
-  chart     = "loki-distributed"
-  version   = "0.69.4"
-  timeout   = 1800
-  wait_for_jobs = true
-  values = [
-    "${file("loki-values.yaml")}"
-  ]
-  cleanup_on_fail = true
-}
+# resource "helm_release" "loki" {
+#   name      = "loki-distributed"
+#   namespace = "default"
+#   create_namespace = true
+#   repository = "https://grafana.github.io/helm-charts"
+#   chart     = "loki-distributed"
+#   version   = "0.69.4"
+#   timeout   = 1800
+#   reuse_values = true
+#   wait = true
+#   wait_for_jobs = true
+#   values = [
+#     "${file("loki-values.yaml")}"
+#   ]
+#   cleanup_on_fail = true
+# }
